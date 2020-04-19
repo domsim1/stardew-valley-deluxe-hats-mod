4f9068f148185591c66acfaf084e00683c2d209d
Domenic <domenicsim1@gmail.com>
2020-04-20 01:21:23 +1000

    Butterfly Bow; Fixed tick issue;

--- DeluxeHats/Hats/ButterflyBow.cs
+++ DeluxeHats/Hats/ButterflyBow.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using StardewValley;
+using StardewValley.BellsAndWhistles;
 
 namespace DeluxeHats.Hats
 {
@@ -7,12 +10,31 @@ namespace DeluxeHats.Hats
         public const string Name = "Butterfly Bow";
         public static void Activate()
         {
-            throw new NotImplementedException();
+            
+            HatService.OnUpdateTicked = (e) =>
+            {
+                if (!Game1.currentLocation.isOutdoors || Game1.player.hasMenuOpen || !Game1.player.canMove || !Game1.game1.IsActive)
+                {
+                    return;
+                }
+                var critters = HatService.Helper.Reflection.GetField<List<Critter>>(Game1.currentLocation, "critters").GetValue();
+                if (critters != null && (e.Ticks%30) == 0 && critters.Count < 340)
+                {
+                    critters.Add(new Butterfly(Game1.player.getTileLocation()));
+                }
+                if (e.Ticks % 480 == 0)
+                {
+                    foreach (var npc in Game1.currentLocation.getCharacters())
+                    {
+                        Game1.player.changeFriendship(5, npc);
+                    }
+                }
+                
+            };
         }
 
         public static void Disable()
         {
-            throw new NotImplementedException();
         }
     }
 }
