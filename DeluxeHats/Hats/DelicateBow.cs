0c45e15c5e127ebc1d6e0292fa4a1fb83fc2a8d5
Domenic Simone <domenicsim1@gmail.com>
2020-05-11 09:33:51 +1000

    version 1.0.4

--- DeluxeHats/Hats/DelicateBow.cs
+++ DeluxeHats/Hats/DelicateBow.cs
@@ -1,10 +1,9 @@
-﻿using System;
-
-namespace DeluxeHats.Hats
+﻿namespace DeluxeHats.Hats
 {
     public static class DelicateBow
     {
         public const string Name = "Delicate Bow";
+        public const string Description = "No effect.";
         public static void Activate()
         {
         }
--- DeluxeHats/Hats/LuckyBow.cs
+++ DeluxeHats/Hats/LuckyBow.cs
@@ -1,10 +1,9 @@
-﻿using System;
-
-namespace DeluxeHats.Hats
+﻿namespace DeluxeHats.Hats
 {
     public  static class LuckyBow
     {
         public const string Name = "Lucky Bow";
+        public const string Description = "No effect.";
         public static void Activate()
         {
         }
